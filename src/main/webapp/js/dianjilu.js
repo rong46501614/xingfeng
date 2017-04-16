e5aa8378921e7e0a6cbfdc4e8cf6f0077229cbee
xingfeng <xingfeng@DESKTOP-KIP91T2>
2017-04-16 21:14:15 +0800

    code commit

---
+++ src/main/webapp/js/dianjilu.js
@@ -0,0 +1,98 @@
+(function() {
+	var $a=$(".js_focus_index a");
+	var $s=$(".js_focus_index a");
+	var cArr=["event_list__item slide__item js_focus_pic p1",
+	          "event_list__item slide__item js_focus_pic p2",
+	          "event_list__item slide__item js_focus_pic p3",
+	          "event_list__item slide__item js_focus_pic p4",
+	          "event_list__item slide__item js_focus_pic p5",
+	          "event_list__item slide__item js_focus_pic p6",
+	          "event_list__item slide__item js_focus_pic p7",
+	          "event_list__item slide__item js_focus_pic p8",
+	          "event_list__item slide__item js_focus_pic p9"];
+	var index=0;
+	function nextimg(){
+		cArr.unshift(cArr[8]);
+		cArr.pop();
+		$("#focus li").each(function(i,e){
+			$(e).removeClass().addClass(cArr[i]);
+		})
+		index++;
+		if (index>8) {
+			index=0;
+		}
+		show();
+	}
+	function previmg(){
+		cArr.push(cArr[0]);
+		cArr.shift();
+		$("#focus li").each(function(i,e){
+			$(e).removeClass().addClass(cArr[i]);
+		})
+		index--;
+		if (index<0) {
+			index=8;
+		}
+		show();
+	}
+	function show(){
+		$($s).removeClass("slide_switch__item--current");
+		$($s).eq(index).addClass("slide_switch__item--current");
+	}
+	$(document).on("click",".p2",function(){
+		previmg();
+		return false;
+	});
+	$(document).on("click",".p4",function(){
+		nextimg();
+		return false;
+	});
+	$("#focus").mouseover(function(){
+		clearInterval(timer);
+	})
+	$("#focus").mouseleave(function(){
+		timer=setInterval(nextimg,10000);
+	})
+	timer=setInterval(nextimg,10000);
+	
+	
+	$("#preLi").click(function(){
+		previmg();
+	});
+	$("#nextLi").click(function(){
+		nextimg();
+	});
+	
+	
+	var j=0;
+	$("#albumlistPreLi").click(function(){
+		j++;
+		if(j<4){
+			var i=0;
+			timer=setInterval(function(){
+				if($("#albumlist").css("left").split("px")[0]>-(1200*j)){
+					i++;
+					$("#albumlist").css("left","-"+ (1200*(j-1) + 10*i) +"px");
+				}else{
+					clearInterval(timer);
+				}
+			},7);
+		}
+	});
+	
+	$("#albumlistNextLi").click(function(){
+		j--;
+		if(j>-1){
+			var i=0;
+			timer=setInterval(function(){
+				if($("#albumlist").css("left").split("px")[0]<-(1200*j)){
+					i++;
+					$("#albumlist").css("left", -(1200*(j+1)-10*i) +"px");
+				}else{
+					clearInterval(timer);
+				}
+			},7);
+		}
+	});
+	
+})();
