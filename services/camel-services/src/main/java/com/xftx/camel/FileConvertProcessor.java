ada497e94e662781290a67bbe70961e1423bbeb0
xingfeng <330563991@qq.com>
2017-12-17 00:44:27 +0800

    setup

--- services/camel-services/src/main/java/com/xftx/camel/FileConvertProcessor.java
+++ services/camel-services/src/main/java/com/xftx/camel/FileConvertProcessor.java
@@ -6,14 +6,18 @@ import java.util.Map;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.springframework.stereotype.Component;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class FileConvertProcessor implements Processor{
+	
+	private static final ObjectMapper OBJECTMAPPER = new ObjectMapper();
 	
     public void process(Exchange exchange) throws Exception {
         Map<String,String> map = new HashMap<String,String>();
         map.put("hello", "world");
         map.put("xingfeng", "world");
+//        String writeValueAsString = OBJECTMAPPER.writeValueAsString(map);
         exchange.getIn().setBody(map);
     }
 }
