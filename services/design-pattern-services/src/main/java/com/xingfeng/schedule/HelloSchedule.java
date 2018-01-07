569170111d66ccfcb3bb39608c913713c4a50a13
xingfeng <330563991@qq.com>
2018-01-07 21:04:48 +0800

    spring schedule

--- services/design-pattern-services/src/main/java/com/xingfeng/schedule/HelloSchedule.java
+++ services/design-pattern-services/src/main/java/com/xingfeng/schedule/HelloSchedule.java
@@ -1,12 +1,18 @@
 package com.xingfeng.schedule;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class HelloSchedule implements Runnable{
 
 	public void run() {
-		System.out.println("hello");
+		Date date = new Date();
+		SimpleDateFormat sim = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String dateStr = sim.format(date);
+		System.out.println("这是spring定时器2，每五秒执行一次,当前时间：" + dateStr);
 	}
 
 }
