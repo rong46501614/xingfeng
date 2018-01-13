5a95a8b91acfb71e6bfd32909bd35874b4432248
xingfeng <330563991@qq.com>
2018-01-14 01:42:40 +0800

    code

--- services/camel-services/src/main/java/com/xftx/camel/FileConvertProcessor.java
+++ services/camel-services/src/main/java/com/xftx/camel/FileConvertProcessor.java
@@ -3,17 +3,49 @@ package com.xftx.camel;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.Endpoint;
+import org.apache.camel.EndpointConfiguration;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.camel.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class FileConvertProcessor implements Processor{
 	
+	@Autowired
+	private ApplicationContext applicationContext;
+	
 	private static final ObjectMapper OBJECTMAPPER = new ObjectMapper();
 	
     public void process(Exchange exchange) throws Exception {
+    	
+    	CamelContext bean = (CamelContext) applicationContext.getBean("testCamelContext");
+    	
+    	Route route = bean.getRoute("tianxia");
+    	
+    	Endpoint endpoint = route.getEndpoint();
+    	
+    	EndpointConfiguration configuration = endpoint.getEndpointConfiguration();
+    	
+    	Object parameter = configuration.getParameter("authority");
+    	
+    	System.out.println(parameter);
+    	
+    	configuration.setParameter("authority", "localhost:9080");
+    	
+    	Object parameters = configuration.getParameter("authority");
+    	
+    	
+    	configuration.getURI();
+    	
+    	System.out.println(parameters);
+    	
         Map<String,String> map = new HashMap<String,String>();
         map.put("hello", "world");
         map.put("xingfeng", "world");
--- services/design-pattern-services/src/main/java/com/xingfeng/schedule/Schedule.java
+++ services/design-pattern-services/src/main/java/com/xingfeng/schedule/Schedule.java
@@ -3,14 +3,27 @@ package com.xingfeng.schedule;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Schedule {
 	
+	@Autowired
+	private ApplicationContext applicationContext;
+	
+	
 	@Scheduled(cron = "0/5 * * * * ?")
 	public void schTest1() {
+		
+		CamelContext bean = (CamelContext) applicationContext.getBean("testCamelContext");
+		
+		Route route = bean.getRoute("xingfeng");
+		
 		Date date = new Date();
 		SimpleDateFormat sim = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		String dateStr = sim.format(date);
